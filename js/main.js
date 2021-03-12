90a710bddd392e3a587e9ad9a910435fd58a0549
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-12 17:32:14 +0100

    Prova definizione funzioni

--- js/main.js
+++ js/main.js
@@ -20,52 +20,33 @@ nextButton.click(
   }
 );
 
-prevButton.click(
-  function() {
-    var selected = $(".active");
-    selected.removeClass("active");
 
-    if (selected.hasClass("first")) {
-      $(".last").addClass("active");
-    }
-    selected.prev().addClass("active");
+function prevScroll() {
+  var selected = $(".active");
+  selected.removeClass("active");
+
+  if (selected.hasClass("first")) {
+    $(".last").addClass("active");
   }
-);
+  selected.prev().addClass("active");
+}
 
 
+
+prevButton.click(prevScroll());
+
+
+// il bullet premuto cambia colore
 $(".nav > i").click(
   function() {
     $(".nav i").removeClass("active");
     $(this).addClass("active");
-
   }
 );
 
-
+// la pagina stampa i tasti premuti
+$(document).keydown(function(evt){
+    alert(evt.keyCode);
+});
 
 // ALLA PRESSIONE DEI TASTI FRECCIA
-
-// .on("keypress")(
-//   function() {
-//     var selected = $(".active");
-//     selected.removeClass("active");
-//
-//     if (selected.hasClass("last")) {
-//       $(".first").addClass("active");
-//     } else {
-//       selected.next().addClass("active");
-//     }
-//   }
-// );
-//
-// $("right").keypress(
-//   function() {
-//     var selected = $(".active");
-//     selected.removeClass("active");
-//
-//     if (selected.hasClass("first")) {
-//       $(".last").addClass("active");
-//     }
-//     selected.prev().addClass("active");
-//   }
-// );
