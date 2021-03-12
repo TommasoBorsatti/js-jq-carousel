b09d14abaca12d8c127d7730fd8a07af803a06ec
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-12 17:18:00 +0100

    Script - Adesso il bullet selezionato si colora
    Penso a un modo per far cambiare la foto in risposta

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ var nextButton = $(".next");
 var prevButton = $(".prev");
 
 
+// AL CLICK
+
 nextButton.click(
   function() {
     var selected = $(".active");
@@ -29,3 +31,41 @@ prevButton.click(
     selected.prev().addClass("active");
   }
 );
+
+
+$(".nav > i").click(
+  function() {
+    $(".nav i").removeClass("active");
+    $(this).addClass("active");
+
+  }
+);
+
+
+
+// ALLA PRESSIONE DEI TASTI FRECCIA
+
+// .on("keypress")(
+//   function() {
+//     var selected = $(".active");
+//     selected.removeClass("active");
+//
+//     if (selected.hasClass("last")) {
+//       $(".first").addClass("active");
+//     } else {
+//       selected.next().addClass("active");
+//     }
+//   }
+// );
+//
+// $("right").keypress(
+//   function() {
+//     var selected = $(".active");
+//     selected.removeClass("active");
+//
+//     if (selected.hasClass("first")) {
+//       $(".last").addClass("active");
+//     }
+//     selected.prev().addClass("active");
+//   }
+// );
